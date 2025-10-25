300663338e57f130f1f9bdb70fa2b11b2adfd732
Benjamin <Benjaminisraeludeh@gmail.com>
2025-10-25 02:59:00 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+alert("hello world");
+console.log("temitope is my friend");
+console.log(40 + 23 + 10);
+console.log(5 ** 5);
+
+let firstName = "Adams";
+console.log(firstName);
+
+let gravity = 9.81;
+const boilingPoint = 100;
+console.log(gravity, boilingPoint);
+
+let age = 20; // declaring age
+console.log(age);
+age = 21; // re-assigning age
+console.log(age);
+
+let fruit = "banana";
+console.log(fruit);
+fruit = "mango";
+console.log(fruit);
+
+//const
+const city = "Lagos";
+console.log(city);
+
+const yearofBirth = 2000;
+const PI = 3.14;
+console.log(yearofBirth, PI);
+
+// var
+var country = "Nigeria";
+console.log(country);
+country = "Ghana";
+console.log(country);
+
+// data types
+
+const a = "A";
+const b = "javaScript is an amazing language";
+console.log(b);
+const c = `we can also use backticks to create string`;
+console.log(c);
+
+//boolean
+const no = false;
+const yes = true;
+console.log(yes, no);
+console.log(2 > 3);
+
+const myObject = {
+  firstName: "Adams",
+  lastName: "Temitope",
+  age: 23,
+  gender: "female",
+  isMarried: false,
+};
+console.log(myObject);
+
+// checking data types
+console.log(typeof "Adams"); // string
+console.log(typeof 23); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(typeof 2 > 4);
