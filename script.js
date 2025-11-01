40ea72dac8946a26b75ff82de210af79106e00d6
Benjamin <Benjaminisraeludeh@gmail.com>
2025-11-01 10:27:35 +0100

    update: new-javascript-fundamentals

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ console.log("temitope is my friend");
 console.log(40 + 23 + 10);
 console.log(5 ** 5);
 
-let firstName = "Adams";
-console.log(firstName);
+// let firstName = "Adams";
+// console.log(firstName);
 
 let gravity = 9.81;
 const boilingPoint = 100;
@@ -24,9 +24,9 @@ console.log(fruit);
 const city = "Lagos";
 console.log(city);
 
-const yearofBirth = 2000;
-const PI = 3.14;
-console.log(yearofBirth, PI);
+// const yearofBirth = 2000;
+// const PI = 3.14;
+// console.log(yearofBirth, PI);
 
 // var
 var country = "Nigeria";
@@ -36,11 +36,11 @@ console.log(country);
 
 // data types
 
-const a = "A";
-const b = "javaScript is an amazing language";
-console.log(b);
-const c = `we can also use backticks to create string`;
-console.log(c);
+// const a = "A";
+// const b = "javaScript is an amazing language";
+// console.log(b);
+// const c = `we can also use backticks to create string`;
+// console.log(c);
 
 //boolean
 const no = false;
@@ -48,14 +48,14 @@ const yes = true;
 console.log(yes, no);
 console.log(2 > 3);
 
-const myObject = {
-  firstName: "Adams",
-  lastName: "Temitope",
-  age: 23,
-  gender: "female",
-  isMarried: false,
-};
-console.log(myObject);
+// const myObject = {
+//   firstName: "Adams",
+//   lastName: "Temitope",
+//   age: 23,
+//   gender: "female",
+//   isMarried: false,
+// };
+// console.log(myObject);
 
 // checking data types
 console.log(typeof "Adams"); // string
@@ -66,3 +66,69 @@ console.log(typeof undefined); // undefined
 console.log(typeof {}); // object
 console.log(typeof []); // object
 console.log(typeof 2 > 4);
+
+//operators
+//arithmetic operators
+//addition
+let a = 10;
+let b = 5;
+let c = a + b;
+console.log(c);
+//subtraction
+const sellingPrice = 750;
+const costPrice = 500;
+const profitValue = sellingPrice - costPrice;
+console.log(profitValue);
+
+// calculating the area of a circle
+const PI = 3.14;
+let radius = 100;
+let areaOfCircle = PI * radius * radius;
+console.log(areaOfCircle);
+
+// string concatenation
+let word_1 = "Hello";
+let word_2 = "World";
+console.log(word_1 + " " + word_2);
+
+let firstName = "Adams";
+let lastName = "Temitope";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+console.log("My" + " name is " + " " + fullName + " ");
+
+// 2. assignment operators
+let d = 2 + 5 + 8 - 2;
+console.log("first", d);
+
+d += 10;
+console.log("second", d);
+d -= 5;
+console.log("third", d);
+d *= 2;
+console.log("fourth", d);
+d /= 4;
+console.log("fifth", d);
+d %= 3;
+console.log("sixth", d);
+
+// 3. comparison operators
+const ageAdult = 18;
+let ageJohn = 25;
+let ageDoe = 30;
+let ageBoolean = ageJohn > ageDoe;
+console.log(ageBoolean);
+console.log(ageJohn >= ageAdult);
+console.log(ageDoe <= ageAdult);
+
+let q = 3;
+let p = "3";
+console.log(q == p); // loose equality
+console.log(q === p); // strict equality
+
+// not equal
+// != loose not equal */
+console.log(2 != 3); // true
+// !== strict not equal */
+console.log(2 !== 3); // true
